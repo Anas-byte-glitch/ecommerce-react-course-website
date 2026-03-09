586cacf73f4488abf7a2879be5c20c6b64a56208
Anas-byte-glitch <mastermindg32@gmail.com>
2026-03-09 23:07:46 +0100

    product details

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
-import Checkout from './pages/Checkout'
+import Checkout from './pages/ProductDetails'
 import AuthProvider from './context/AuthContext'
+import ProductDetails from './pages/ProductDetails'
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
           <Route path='/' element={<Home />}/>
           <Route path='/auth' element={<Auth />}/>
           <Route path='/checkout' element={<Checkout />}/>
+          <Route path='/products/:id' element={<ProductDetails />}/>
         </Routes>
       </div>
     </AuthProvider>
--- src/pages/Checkout.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Checkout() {
-  return (
-    <div>
-      Checkout Page
-    </div>
-  );
-}
